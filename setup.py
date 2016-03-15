8af34b00637391ab240b9244bd50311833b32899
root <root@ip-172-31-34-234.eu-west-1.compute.internal>
2016-03-15 15:23:25 +0000

    gevent setuptools

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     packages=find_packages(),
 
     install_requires=[
-        "gevent>=1.0.1",
+        "gevent>=1.0.2",
         "nylas-production-python>=0.2.0",
         "click>=2.4",
         "cpu_affinity>=0.1.0",
@@ -46,6 +46,7 @@ setup(
         "backports.ssl>=0.0.9",
         "imapclient==1.0.1",
         "tldextract==1.7.5",
+	"setuptools==20.1.1",
     ],
     dependency_links=["git+https://github.com/nylas/dateutil.git@da336a366cd3#egg=python-dateutil-2.4.2"],
 
