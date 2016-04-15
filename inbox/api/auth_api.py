86620392d51bb1750fa50b1bfe846d1aaa99eced
cirk0 <mark@ecasework.com>
2016-04-15 12:03:06 +0100

    Revert "remove basic auth"
    This reverts commit 5c9dd21b7f642d2685f21f36bb8b5e0877d9ed66.

--- inbox/api/auth_api.py
+++ inbox/api/auth_api.py
@@ -45,16 +45,15 @@ app = Blueprint(
     
 @app.before_request
 def start():
-#   engine = engine_manager.get_for_id(g.namespace_id)
-#   g.db_session = new_session(engine)
-#   g.namespace = Namespace.get(g.namespace_id, g.db_session)
+    engine = engine_manager.get_for_id(g.namespace_id)
+    g.db_session = new_session(engine)
+    g.namespace = Namespace.get(g.namespace_id, g.db_session)
 
-#   g.log = log.new(endpoint=request.endpoint,
-#       account_id=g.namespace.account_id)
-   g.parser = reqparse.RequestParser(argument_class=ValidatableArgument)
-   g.encoder = APIEncoder()
-   return
-
+    g.log = log.new(endpoint=request.endpoint,
+        account_id=g.namespace.account_id)
+    g.parser = reqparse.RequestParser(argument_class=ValidatableArgument)
+    g.encoder = APIEncoder()
+    
 @app.after_request
 def finish(response):
     if response.status_code == 200 and hasattr(g, 'db_session'):  # be cautions
